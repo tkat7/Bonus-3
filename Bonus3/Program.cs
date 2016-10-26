34c95a8d619c42b6d83586e83d4d236c1e138828
tkat7 <tkatona2013@gmail.com>
2016-10-26 12:36:57 -0400

    finish validation method

--- Bonus3/Program.cs
+++ Bonus3/Program.cs
@@ -10,19 +10,62 @@ namespace Bonus3
     {
         static void Main(string[] args)
         {
-            Boolean safe = true;
+            Boolean safe = false;
             Boolean check = false;
             Random rnd = new Random();
             int answer = rnd.Next(1, 100);
             do
             {
                 int n;
-                Console.WriteLine("Guess a number 1-100");
-                string input = Console.ReadLine();
-                safe = int.TryParse(input, out n);
-                while (safe == true);
+                while (safe == false)
+                {
+                    Console.WriteLine("Guess a number 1-100");
+                    string input = Console.ReadLine();
+                    safe = int.TryParse(input, out n);
+                }
                 int guess = int.Parse(input);
+
+
+
+
+                int status = 1;
+                switch (status)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+                        break;
+                }
             } while (check == false);
+        }
+
+        public static Boolean CheckGuess(int guess, int num)
+        {
+            if (guess >= num + 10)
+            {
+                Console.WriteLine("That is way too high");
+                return false;
+            }
+            else if (guess <= num - 10)
+            {
+                Console.WriteLine("That is way too low");
+                return false;
+            }
+            else if (guess > num)
+            {
+                Console.WriteLine("That is too high");
+                return false;
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("That is too low");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
