c1638471c4fa65ac36597fbcfca33e1585701b81
tkat7 <tkatona2013@gmail.com>
2016-10-26 09:20:39 -0400

    first commit

---
+++ Bonus3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int answer = rnd.Next(1, 100);
+            Console.Write("Guess a number 1-100");
+
+        }
+    }
+}
